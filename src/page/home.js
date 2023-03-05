c7b95d7793b97acc458054b7f61e51755ba2bfba
Ugo De Lucia <deluciaugo@gmail.com>
2023-03-06 00:44:00 +0100

    aggiunto badge per la priority

--- src/page/home.js
+++ src/page/home.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Row, Col, ListGroup, Form } from 'react-bootstrap';
+import { Container, Row, Col, ListGroup, Form, Badge } from 'react-bootstrap';
 import Header from '../component/header';
 import FormAddTask from '../component/task/formAddTask';
 import { getAllTasks, getTask, modifyTask } from '../db/database';
@@ -23,7 +23,7 @@ const Home = (props) => {
         const fetchTasks = async () => {
             const allTasks = await getAllTasks();
             if (allTasks) {
-                setTasks(allTasks);
+                setTasks(allTasks)
             }
         };
         fetchTasks();
@@ -43,18 +43,28 @@ const Home = (props) => {
                     {tasks.length > 0 &&
                         <ListGroup>
                             {tasks.map((task, index) => (
-                                <ListGroup.Item key={task._id} className="mb-4 d-flex justify-content-between align-items-center"
-                                    style={{
-                                        textDecoration: task.isDone ? "line-through" : "none",
-                                    }}>
-                                    {task.description}
-                                    <Form.Check
-                                        inline
-                                        type="checkbox"
-                                        checked={task.isDone}
-                                        onChange={() => updateTask(task._id, task.isDone)}
-                                    />
-                                </ListGroup.Item>
+                                <>
+                                    <Badge
+                                        bg={ task.priority === 'low' ? 'success' : task.priority === 'medium' ? 'primary': 'danger'}
+                                        style={{width: '100px',marginLeft: "auto",marginBottom:"10px"}}
+                                    >
+                                        {task.priority}
+                                    </Badge>
+                                    <ListGroup.Item key={task._id} className="mb-4 d-flex justify-content-between align-items-center"
+                                        style={{
+                                            textDecoration: task.isDone ? "line-through" : "none",
+                                            border: task.priority === "low" ? "1px solid green" : task.priority === "medium" ? "1px solid blue" : "1px solid red",
+                                            borderRadius: 0,
+                                        }}>
+                                        {task.description}
+                                        <Form.Check
+                                            inline
+                                            type="checkbox"
+                                            checked={task.isDone}
+                                            onChange={() => updateTask(task._id, task.isDone)}
+                                        />
+                                    </ListGroup.Item>
+                                </>
                             ))}
                         </ListGroup>
                     }
